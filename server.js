1bc3120fb800f3f8fe7b90accc1d238fa584c960
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-17 17:05:51 -0400

    Express js setup

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
 const PORT = 3000;
 
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Working Title',
+            version: '1.0.0',
+            description: 'A simple Express API',
+        },
+    },
+    apis:['./routes/*.js']
+};
+
+const swaggerDocument = swaggerJsDoc(swaggerOptions);
+
 app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
