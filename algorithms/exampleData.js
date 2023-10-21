546e9d8220803238779c46a0e43d501f120876e2
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-20 23:57:21 -0400

    Example polyline and elevation data populated.

---
+++ algorithms/exampleData.js
@@ -0,0 +1,19 @@
+const polylinePoints = [
+    // Starting Point: Los Angeles, CA
+    { lat: 34.0522, lng: -118.2437 },
+    { lat: 34.0525, lng: -118.2440 },
+    { lat: 34.0528, lng: -118.2443 },
+    { lat: 34.0531, lng: -118.2446 },
+    { lat: 34.0534, lng: -118.2449 },
+    { lat: 34.4180, lng: -119.6990 }
+    // End Point: Santa Barbara, CA
+];
+
+const elevationData = [
+    { location: { lat: 34.0522, lng: -118.2437 }, elevation: 100.0 },
+    { location: { lat: 34.0525, lng: -118.2440 }, elevation: 102.5 },
+    { location: { lat: 34.0528, lng: -118.2443 }, elevation: 105.0 },
+    { location: { lat: 34.0531, lng: -118.2446 }, elevation: 107.5 },
+    { location: { lat: 34.0534, lng: -118.2449 }, elevation: 110.0 },
+    { location: { lat: 34.4180, lng: -119.6990 }, elevation: 120.0 }
+];
