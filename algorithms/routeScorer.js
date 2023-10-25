d3f6ac46d2a8562d888c2e7604cc7044d8592906
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-24 20:54:56 -0400

    Update to calculate curvature of polyline points.

--- algorithms/routeScorer.js
+++ algorithms/routeScorer.js
@@ -1,22 +1,39 @@
 function computeAngle(p1, p2, p3) {
-    const angleRad = Math.atan2(p3.lat - p2.lat, p3.lng - p2.lng) -
-                    Math.atan2(p1.lat - p2.lat, p1.lng - p2.lng);
+    const angleRad = Math.atan2(p3.lng - p1.lng, p3.lat - p1.lat) -
+                    Math.atan2(p2.lng - p1.lng, p2.lat - p1.lat);
     return Math.abs(angleRad * (180 / Math.PI));
 }
 
-function isFunRoad(points, angleThreshold = 30, percentageThreshold = 0.5) {
-    let angleExceedCount = 0;
-
-    for (let i = 1; i < points.length - 1; i++) {
-        const angle = computeAngle(points[i-1], points[i], points[i+1]);
-        if (angle > angleThreshold) {
-            angleExceedCount++;
-        }
-    }
-
-    const percentageExceed = angleExceedCount / (points.length - 2);
-    return percentageExceed > percentageThreshold;
+function computeDistance(p1, p2) {
+    const dx = p2.lng - p1.lng;
+    const dy = p2.lat - p1.lat;
+    return Math.sqrt(dx * dx + dy * dy);
 }
 
-const result = isFunRoad(polylinePoints);
-console.log(result ? "Fun Road" : "Not Fun Road");
+function computeCurvature(polylinePoints) {
+    const curvatures = [];
+    for(let i = 0; i < polylinePoints.length - 2; i++) {
+        const p1 = polylinePoints[i];
+        const p2 = polylinePoints[i+1];
+        const p3 = polylinePoints[i+2];
+
+        const angle = computeAngle(p1, p2, p3);
+        const ds = computeDistance(p1, p3);
+
+        const curvature = angle / ds;
+        curvatures.push(curvature);
+    }
+    return curvatures;
+}
+
+const polylinePoints = [/*... your data ...*/];
+const curvatures = computeCurvature(polylinePoints);
+
+function isRoadFun(curvatures, thresholdAngle = 30, minFunPercent = 0.5) {
+    const funSegments = curvatures.filter(curvature => curvature > thresholdAngle);
+    const funFactor = funSegments.length / curvatures.length;
+    return funFactor > minFunPercent;
+}
+
+const fun = isRoadFun(curvatures);
+console.log(`Is the road fun? ${fun ? 'Yes' : 'No'}`);
