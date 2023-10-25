60f2d6c389debf8386c4fc779fb571de6bacae48
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-24 23:22:00 -0400

    Variations to example polylines added.

--- algorithms/exampleData.js
+++ algorithms/exampleData.js
@@ -1,11 +1,11 @@
 const polylinePoints = [
     // Starting Point: Los Angeles, CA
-    { lat: 34.0522, lng: -118.2437 },
-    { lat: 34.0525, lng: -118.2440 },
-    { lat: 34.0528, lng: -118.2443 },
-    { lat: 34.0531, lng: -118.2446 },
-    { lat: 34.0534, lng: -118.2449 },
-    { lat: 34.4180, lng: -119.6990 }
+    { lat: 31.0522, lng: -118.2437 },
+    { lat: 32.0525, lng: -119.2440 },
+    { lat: 33.0528, lng: -120.2443 },
+    { lat: 34.0531, lng: -121.2446 },
+    { lat: 35.0534, lng: -122.2449 },
+    { lat: 36.4180, lng: -123.6990 }
     // End Point: Santa Barbara, CA
 ];
 
@@ -17,3 +17,5 @@ const elevationData = [
     { location: { lat: 34.0534, lng: -118.2449 }, elevation: 110.0 },
     { location: { lat: 34.4180, lng: -119.6990 }, elevation: 120.0 }
 ];
+
+module.exports = polylinePoints;
