ec9e34758d5b9d45ef785248c2414c631ed5e1c1
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-24 23:22:51 -0400

    Tested, working with example data.

--- algorithms/routeScorer.js
+++ algorithms/routeScorer.js
@@ -4,9 +4,9 @@ function computeAngle(p1, p2, p3) {
     return Math.abs(angleRad * (180 / Math.PI));
 }
 
-function computeDistance(p1, p2) {
-    const dx = p2.lng - p1.lng;
-    const dy = p2.lat - p1.lat;
+function computeDistance(p1, p3) {
+    const dx = p3.lng - p1.lng;
+    const dy = p3.lat - p1.lat;
     return Math.sqrt(dx * dx + dy * dy);
 }
 
@@ -26,7 +26,14 @@ function computeCurvature(polylinePoints) {
     return curvatures;
 }
 
-const polylinePoints = [/*... your data ...*/];
+const polylinePoints = [
+    { lat: 10.0522, lng: -18.2437 },
+    { lat: 70.0525, lng: -119.2440 },
+    { lat: 20.0528, lng: -20.2443 },
+    { lat: 40.0531, lng: -121.2446 },
+    { lat: 45.0534, lng: -22.2449 },
+    { lat: 11.4180, lng: -123.6990 }
+];
 const curvatures = computeCurvature(polylinePoints);
 
 function isRoadFun(curvatures, thresholdAngle = 30, minFunPercent = 0.5) {
@@ -37,3 +44,5 @@ function isRoadFun(curvatures, thresholdAngle = 30, minFunPercent = 0.5) {
 
 const fun = isRoadFun(curvatures);
 console.log(`Is the road fun? ${fun ? 'Yes' : 'No'}`);
+console.log(curvatures)
+console.log(fun)
